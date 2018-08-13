2e83f5446f493aabce09f76ff6f6075dc4d076ef
Marcin Czekala <marcin_czekala@icloud.com>
2018-08-13 17:47:20 +0200

    add

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 var fs = require('fs');
-var fs = require('fs');
 var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
-var stringifyFile;
+var stringifyFile = '';
 
 app.use(bodyParser.json());
 
-app.get('/getNote', function(req, res) {
-  fs.readFile('./test.json', 'utf8', function(err, data) {
-    if(err) throw err;
+app.get('/getNote', function (req, res) {
+  fs.readFile('./test.json', 'utf8', function (err, data) {
+    if (err) throw err;
     stringifyFile = data
     res.send(data);
   });
 });
-app.post('/updateNote/:note', function(req, res) {
-  console.log(req.params.note);
-  fs.writeFile('./test.json', stringifyFile, function(err) {
-    if(err) throw err;
+app.post('/updateNote/:note', function (req, res) {
+  stringifyFile += req.params.note;
+  fs.writeFile('./test.json', stringifyFile, function (err) {
+    if (err) throw err;
     console.log('file updated');
   });
 });
-var server = app.listen(3000, function() {
+var server = app.listen(3000, function () {
   console.log('listening...');
 });
